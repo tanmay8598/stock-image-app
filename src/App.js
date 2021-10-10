f94716a1d448bf17e1fc1d58ce2b018b1f7a6724
Tanmay <you@example.com>
2021-10-10 15:49:02 +0530

    infinite acroll and search query

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import Photo from './Photo'
+import { FaSearch } from 'react-icons/fa'
 const mainUrl = 'https://api.unsplash.com/photos/'
+const searchUrl = 'https://api.unsplash.com/search/photos/'
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 
 const App = () => {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(0)
+  const [query, setQuery] = useState('')
   const fetchImage = async () => {
     setLoading(true)
     let url
-    url = `${mainUrl}${clientID}`
+    const urlPage = `&page=${page}`
+    const urlQuery = `&query=${query}`
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`
+    }
     try {
       const response = await fetch(url)
       const data = await response.json()
-      console.log(data)
-      setPhotos(data)
+      // console.log(data)
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results
+        } else if (query) {
+          return [...oldPhotos, ...data.results]
+        } else {
+          return [...oldPhotos, ...data]
+        }
+      })
       setLoading(false)
     } catch (error) {
       setLoading(false)
@@ -23,12 +41,42 @@ const App = () => {
   }
   useEffect(() => {
     fetchImage()
-  }, [])
+    // eslint-disable-next-line
+  }, [page])
+
+  useEffect(() => {
+    const event = window.addEventListener('scroll', () => {
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => {
+          return oldPage + 1
+        })
+      }
+    })
+    return () => window.removeEventListener('scroll', event)
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setPage(1)
+    fetchImage()
+  }
+
   return (
     <div className='app-wrapper'>
       <div className='search-container'>
-        <input type='text' />
-        <button>submit</button>
+        <input
+          type='text'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder='Search'
+        />
+        <button className='btn' onClick={handleSubmit}>
+          {' '}
+          <FaSearch />
+        </button>
       </div>
       <div className='photo-container'>
         {photos.map((image, index) => {
--- src/Photo.js
+++ src/Photo.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
-const Photo = ({ urls: { regular } }) => {
+const Photo = ({
+  urls: { regular },
+  alt_description,
+  likes,
+  user: {
+    name,
+    portfolio_url,
+    profile_image: { medium },
+  },
+}) => {
   return (
     <div className='photo-wrapper'>
-      <img src={regular} alt='' />
+      <img src={regular} alt={alt_description} className='image' />
+      <div className='photo-info'>
+        <div>
+          {' '}
+          <h4>{name}</h4>
+          <p>{likes} likes</p>
+        </div>
+        <a href={portfolio_url}>
+          <img src={medium} alt='' className='user-img' />
+        </a>
+      </div>
     </div>
   )
 }
